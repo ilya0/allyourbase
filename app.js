32034e3633347a44400f9d88f144c9e04af1f85e
ilya o <iosovets@gmail.com>
2018-03-01 20:14:47 -0800

    body parser type shits"
    æ

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
-var http = require('http'); 
+// var http = require('http'); 
 
-var fs = require('fs');
+// var fs = require('fs');
 
-var jsonData = {
-    'name': 'NodeJS',
-    'lang': 'Javascript',
-    'use': 'Write once, run everywhere'
-};
+// var jsonData = {
+//     'name': 'NodeJS',
+//     'lang': 'Javascript',
+//     'use': 'Write once, run everywhere'
+// };
 
 
-http.createServer(function (req, res) {
-    fs.readFile('./index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-});
+// http.createServer(function (req, res) {
+//     fs.readFile('./index.html', function(err, data) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write(data);
+//     res.end();
+// });
 
        
-    }).listen(3000);
-    console.log("We are no listening on port 3000");
+//     }).listen(3000);
+//     console.log("We are no listening on port 3000");
+
+
+
+//now using express
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var urlencodedParser = bodyParser.urlencoded({extended : false});
+app.use(urlencodedParser);
+app.set('view engine', 'ejs');
+app.get("/", function(req,res){
+                res.render("index");
+        });
+app.get("/contact-us", function(req,res){
+                res.render("contact");
+        });
+app.post("/contact-us", function(req,res){
+                res.write("You have contacted us. Details are below");
+                res.write("\nHi "+ req.body.firstname + ", you said:");
+                res.write("\nMessage: "+ req.body.message)
+                res.write("\nWe would use your provided email "+ req.body.email+ " to get back to you");
+                res.end();
+        });
+app.listen(8080, 'localhost');
+console.log("We are now listening on port 8080"); //Helps us know our app is actually running
