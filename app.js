44e3710a4682d07a76125d4f9e7b0a0b03bb2205
ilya o <iosovets@gmail.com>
2018-03-01 19:57:48 -0800

    serving html

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var http = require('http'); 
 
+var fs = require('fs');
+
 var jsonData = {
     'name': 'NodeJS',
     'lang': 'Javascript',
@@ -7,9 +9,13 @@ var jsonData = {
 };
 
 
-   http.createServer(function (req, res) {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.write('Hello World!');
-        res.end(JSON.stringify(jsonData));
+http.createServer(function (req, res) {
+    fs.readFile('./index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+});
+
+       
     }).listen(3000);
     console.log("We are no listening on port 3000");
